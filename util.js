a25bc71df3b211669194f8e75e3f77c3e5ad41ec
Ousjiali <69753306+Ousjiali@users.noreply.github.com>
2021-04-02 21:17:13 +0100

    Add files via upload

---
+++ util.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import numeral from 'numeral'
+import { Circle, Popup } from 'react-leaflet'
+
+const casesTypeColors = {
+  cases: {
+    hex: '#CC1222',
+    multiplier: 800,
+  },
+  recovered: {
+    hex: '#7dd71d',
+    multiplier: 700,
+  },
+  deaths: {
+    hex: '#fb4443',
+    multiplier: 1800,
+  },
+}
+
+export const sortData = (data) => {
+  const sortedData = [...data]
+
+  //return sortedData.sort((a,b) => (a.cases > b.cases ? -1 : 1)) or
+
+  sortedData.sort((a, b) => {
+    if (a.cases > b.cases) {
+      return -1
+    } else {
+      return 1
+    }
+  })
+  return sortedData
+}
+
+export const prettyPrintStat = (stat) =>
+  stat ? `+${numeral(stat).format('0.0a')}` : '+0'
+
+export const showDataOnMap = (data, casesType = 'cases') =>
+  data.map((country, index) => (
+    <Circle
+      key={index}
+      center={[country.countryInfo.lat, country.countryInfo.long]}
+      fillOpacity={0.4}
+      pathOptions={{
+        color: casesTypeColors[casesType].hex,
+        fillColor: casesTypeColors[casesType].hex,
+      }}
+      radius={
+        Math.sqrt(country[casesType] / 10) *
+        casesTypeColors[casesType].multiplier
+      }
+    >
+      <Popup>
+        <div className='info-container'>
+          <div
+            className='info-flag'
+            style={{ backgroundImage: `url(${country.countryInfo.flag})` }}
+          />
+          <div className='info-name'>{country.country}</div>
+          <div className='info-confirmed'>
+            Cases: {numeral(country.cases).format('0,0')}
+          </div>
+          <div className='info-recovered'>
+            Recovered: {numeral(country.recovered).format('0,0')}
+          </div>
+          <div className='info-deaths'>
+            Deaths: {numeral(country.deaths).format('0,0')}
+          </div>
+        </div>
+      </Popup>
+    </Circle>
+  ))
